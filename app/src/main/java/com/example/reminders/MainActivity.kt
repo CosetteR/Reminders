18616e5d3c0290448aa7c181fd3dcfbc8aa55821
dyao0210 <dyao0210@users.noreply.github.com>
2022-02-08 21:27:31 -0500

    some stuff
    some changes

--- app/src/main/java/com/example/reminders/MainActivity.kt
+++ app/src/main/java/com/example/reminders/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.reminders
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import org.w3c.dom.Text
 
 var listsList = ArrayList<List>()
@@ -17,11 +15,23 @@ class MainActivity : AppCompatActivity() {
     lateinit var myList: TextView
     lateinit var lists: ListView
     lateinit var add: Button
+    var listOfLists = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lists = findViewById(R.id.lists)
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfLists)
+        lists.adapter = arrayAdapter
+
+        lists.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+
+            //val intent = Intent(this, List::class.java)
+            //intent.putExtra("Who", i)
+            //startActivity(intent)
+
+        }//lists.onItemClickListener
 
     }//onCreate
 
@@ -40,9 +50,13 @@ class MainActivity : AppCompatActivity() {
         builder.setPositiveButton("Save", DialogInterface.OnClickListener { dialog, which ->
             // Here you get get input text from the Edittext
             var title = input.text.toString()
-            var emptyList = ArrayList<String>()
-            var titleList = List(title, emptyList)
-            listsList.add(titleList)
+            //var emptyList = ArrayList<String>()
+           // var titleList = List(title, emptyList)
+            //listsList.add(titleList)
+            listOfLists.add(title)
+
+            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfLists)
+            lists.adapter = arrayAdapter
         })
         builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
 
